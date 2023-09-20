f99d190ade74def69a6abb9cc03a6e0b20bc1135
jamesndungu <jimmindungu3@gmail.com>
2023-09-20 12:40:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun("play table tennis");
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = function(wrapper = `"*"`) {
+    return function(adjective = "special") {
+        return `You are ${wrapper}${adjective}${wrapper}!` 
+    }
+}
+
+wrapAdjective("%")("a dedicated programmer!")
